0b0fbbdeb11fb48456f39449cd597453dd73b3bf
kelvin-ndoma <kelvinndomamutua@gmail.com>
2023-08-22 14:02:51 +0300

    integers and floats examples

---
+++ python.py
@@ -0,0 +1,32 @@
+# a step by step coding
+# variables creation
+# assign a name to the variabe then give it a value
+
+# Numbers
+
+# name = value
+
+x_position = 10 
+# here we created a variable name and assigned a value to it
+print(x_position)
+
+# reassign a value now to the position of x
+x_position = 15
+print(x_position)
+
+# note this print the previous value and the latest value
+
+
+#  float
+pi = 3.14
+pi = 3.13159
+
+print(pi)
+
+# note we can change the type of x to float by
+x_position = 15
+print(x_position)
+
+# to now check the type of x_position
+
+print(type(x_position))
