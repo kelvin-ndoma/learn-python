7edad50c86f380cd40521c3a66c49abe4e65c896
kelvin-ndoma <kelvinndomamutua@gmail.com>
2023-08-22 14:27:14 +0300

    booleans and strings

---
+++ boolean.py
@@ -0,0 +1,39 @@
+# relate to game development
+# Boolean
+
+is_game_over = False
+is_game_over = True
+
+# This will print the value assigned to it
+print(is_game_over)
+
+# again check the type
+
+print(type(is_game_over))
+
+# note you can as well change the type of the data to a different type as follow
+
+is_game_over = 1
+print(is_game_over)
+
+# again check the type
+# check the value of the output now
+print(type(is_game_over))
+
+# list of characters
+# always in quotes
+
+name = "Kelvin"
+
+is_game_over_text = "False"
+age_as_a_string = "5"
+print(name)
+print(is_game_over_text)
+print(type(name))
+
+# concatation
+# combining a string and a number
+# the {} is for passing stored values that we dont know
+age = 27
+name_and_age = "Kelvin is: {}".format(age)
+print(name_and_age)
---
+++ strings.py
@@ -0,0 +1,4 @@
+# list of characters
+# always in quotes
+
+name = "Kelvin"
